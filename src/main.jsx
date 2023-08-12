226275430a9c39f0ab95b0389c4081537908bea6
DoSon <sonnvc2001@gmail.com>
2023-08-12 11:15:08 +0700

    setup styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import App from './App.jsx'
-import './index.css'
+import { GlobalStyles } from './styles/GlobalStyles'
+import { theme } from './utils/constant'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 )
